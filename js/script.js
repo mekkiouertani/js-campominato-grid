5bcdde493866ba592440eb8e273debc41865ff0a
Mekki Ouertani <140953094+mekkiouertani@users.noreply.github.com>
2023-10-18 15:43:55 +0200

    add comments to js, updated size #playground

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//creiamo la base su html con div che sarà la scacchiera e un bottone
+//OBIETTIVO: al click del bottone dobbiamo stampare dei quadrati per formare una scacchiera
+//creiamo una funzione che creetà i vari quadrati
+//dichiariamo il campo da gioco (playground) dentro il la funzione del click
+//creiamo ciclo for per far stampare i quarati
+//aggiungiamo classi per dare effetti ai quadrati
+
+
+const btn = document.querySelector('button');
+console.log(btn);
