40196f62dae41b0d4c7b05b8fbce7fc4ca133578
Mekki Ouertani <140953094+mekkiouertani@users.noreply.github.com>
2023-10-18 16:16:45 +0200

    add const to js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 //creiamo la base su html con div che sarà la scacchiera e un bottone
 //OBIETTIVO: al click del bottone dobbiamo stampare dei quadrati per formare una scacchiera
 //creiamo una funzione che creetà i vari quadrati
+    //dichiarare il numero di quadratini che vogliamo
+    //inserire la formula della radice quadrata
+    //
 //dichiariamo il campo da gioco (playground) dentro il la funzione del click
 //creiamo ciclo for per far stampare i quarati
 //aggiungiamo classi per dare effetti ai quadrati
@@ -12,15 +15,24 @@ const btn = document.querySelector('button');
 btn.addEventListener('click', function(){
     //dichiariamo il div dove stamperemo i nostri quadrati
     const playGround = document.getElementById('playground');
-
+    //evitiamo di stampare infiniti campi da gioco
+    playGround.innerHTML = '';
+    //dichiariamo il numero di quadratini che vogliamo
+    const numSquare = 100;
+    
+    //creiamo il ciclo for per far stampare i 100 quadratini
+    
+    
 });
 
 //funzione per stampare i quadrati
 function drawSquare(){
+    //formula per il quadrato
+    const squareWidth = Math.sqrt(numSquare);
     //creiamo il div che diventerà un quadrato
     const square = document.createElement('div');
     //aggiungiamo la classe con i bordi per vedere il div
     square.classList.add('square');
     //diamo una dimensione al div per farlo diventare un quadrato
-    square.style.width = //abbiamo bisogno di sapere il numero di caselle da creare
+    square.style.width = `calc(100% / ${squareWidth})`;
 };
